48f457a43746cd288d0bacccfdd8ae7876ff385e
github akito-10 <akt.fukuda@gmail.com>
2021-06-29 07:20:42 +0900

    corsの許可

--- app/app.js
+++ app/app.js
@@ -4,6 +4,21 @@ const PORT = process.env.PORT || 8000;
 const Practice = require("./api/models/practiceModel");
 const mongoose = require("mongoose");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  if ("OPTIONS" === req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
 mongoose.Promise = global.Promise;
 const connectDB = require("./db");
 connectDB();
