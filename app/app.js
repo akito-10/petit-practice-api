9f095b62a044ea29017aefb85464e5cc685e7309
github akito-10 <akt.fukuda@gmail.com>
2021-07-03 23:45:49 +0900

    fix: cors設定の修正

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ const corsOption = {
   origin: ["http://127.0.0.1:5501"],
   credentials: true,
 };
-app.use(corsOption);
+app.use(cors(corsOption));
 
 mongoose.Promise = global.Promise;
 const connectDB = require("./db");
